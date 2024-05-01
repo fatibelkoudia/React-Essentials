c0678c3e2b5bda7f88c10aeb38b40bfaae44d1fd
belkoudia fatima-ezzahra <fatima.ezzahra.belkoudia@gmail.com>
2024-05-01 17:22:29 +0200

    Rendering Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import {EXAMPLES} from "./data.js";
 
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] =useState('components');
+  const [ selectedTopic, setSelectedTopic ] =useState();
 
   
   function handleSelect(selectedButton) {
@@ -39,15 +39,18 @@ function App() {
           <TabButton onSelect={()=> handleSelect('props')}> Props </TabButton>
           <TabButton onSelect={()=> handleSelect('state')}> State </TabButton>
         </menu>
-       <div id="tab-content"> 
-        <h3>{EXAMPLES[selectedTopic].title} </h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-          {EXAMPLES[selectedTopic].code}
-          </code>
-        </pre>
-       </div>
+        {!selectedTopic ?<p>Please select a topic.</p> :(<div id="tab-content"> 
+       
+
+       <h3>{EXAMPLES[selectedTopic].title} </h3>
+       <p>{EXAMPLES[selectedTopic].description}</p>
+       <pre>
+         <code>
+         {EXAMPLES[selectedTopic].code}
+         </code>
+       </pre>
+      </div>  ) } 
+       
         </section>
       </main>
     </div>
